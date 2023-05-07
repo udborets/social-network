787b11d391c3af17dd98355b5b380d9f00bf9af6
Yury Borets <udborets@gmail.com>
2023-05-07 22:33:11 +0300

    add test route

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
+import { db } from "./db";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/:id/a", (req, res) => {
-  res.send(req.json());
+app.get("/get", async (req, res) => {
+  const allUsers = await db.user.findMany();
+  res.send(JSON.stringify(allUsers));
 });
+
+app.listen(3000, () => {
+  console.log('listening http://localhost:3000')
+})
