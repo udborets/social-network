d354c63e886575061d4ae5301465a4996fd093a1
Yury Borets <udborets@gmail.com>
2023-05-07 15:24:49 +0300

    init

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+
+const HomePage = () => {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+    >
+    </main>
+  )
+}
+
+export default HomePage;
