daed979983f22b99f69680068d0b429bea0fb557
Yury Borets <udborets@gmail.com>
2023-05-08 10:37:34 +0300

    add react query provider

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import '@/styles/global.scss';
+
+const client = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <QueryClientProvider client={client}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </>
   )
 }
