28da58b39857b38759c4889faeed24e3031009f6
Yury Borets <udborets@gmail.com>
2023-05-08 19:08:48 +0300

    fix user id page

--- client/pages/user/[id]/index.tsx
+++ client/pages/user/[id]/index.tsx
@@ -1,22 +1,46 @@
 'use client'
 
-import UserInfo from "@/features/userIdPage/UserInfo/UserInfo";
-import { userState } from "@/store/User";
+import axios from "axios";
 import { observer } from "mobx-react-lite";
+import Link from "next/link";
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { DBUser } from "@/db/models";
+import { userState } from "@/store/User";
+import PostField from "@/features/PostField/PostField";
+import BackToMyPageButton from "@/features/BackToMyPageButton/BackToMyPageButton";
 
 const UserIdPage = observer(() => {
   const router = useRouter();
-  const [userId, setUserId] = useState<string>('');
-  useEffect(() => {
-    const id = router.query.id as string;
-    setUserId(id);
-    console.log(id)
-  }, [router.isReady])
+  const { data: userInfo } = useQuery({
+    queryFn: async () => {
+      const id = router.query.id as string;
+      try {
+        const url = process.env.NEXT_PUBLIC_BACKEND_URL ?? ""
+        const fetchedUser = await axios.post<{ user: DBUser, OK: boolean }>(url + `/users/id`, { id })
+        if (fetchedUser.data.OK) {
+          return fetchedUser.data.user;
+        }
+      }
+      catch (e) {
+        console.error(e);
+      }
+    },
+    queryKey: [`${router.query.id}`]
+  })
+  if (!userInfo?.id) {
+    return (
+      <main className="w-full h-full grid place-items-center font-bold text-[1.5rem]">
+        <BackToMyPageButton />
+      </main>
+    )
+  }
   return (
-    <main>
-      <UserInfo userId={userId} />
+    <main className="w-full h-full flex flex-col">
+      {userInfo.name}
+      {userInfo.id === userState.info.id
+        ? <PostField />
+        : ''}
     </main>
   )
 })
