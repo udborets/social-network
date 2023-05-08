1996c84f32db646b8a8112ad2aabd84490aacbce
Yury Borets <udborets@gmail.com>
2023-05-08 21:04:48 +0300

    fix posts in posts page

--- client/pages/posts/index.tsx
+++ client/pages/posts/index.tsx
@@ -18,9 +18,12 @@ const PostsPage = () => {
   })
   return (
     <main className="w-full h-full flex flex-col gap-4">
-      {posts ? posts.sort((a, b) => (new Date(a.createdAt ?? Date.now())).getTime() - (new Date(b.createdAt ?? Date.now())).getTime()).map((postProps) => (
-        <Post {...postProps} key={postProps.id} />
-      ))
+      {posts ? posts
+        .sort((a, b) => (new Date(a.createdAt ?? Date.now())).getTime()
+          - (new Date(b.createdAt ?? Date.now())).getTime())
+        .map((postProps) => (
+          <Post {...postProps} key={postProps.id} />
+        ))
         : ''}
     </main>
   )
