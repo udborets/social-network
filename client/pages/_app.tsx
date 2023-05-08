f6b730d43a34fdf3ec681a9aa1bce542255db9d0
Yury Borets <udborets@gmail.com>
2023-05-08 15:46:43 +0300

    add user state changing on app load

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,24 @@
+import { observer } from 'mobx-react-lite';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { useLocalStorage } from 'usehooks-ts';
 
+import { DBUser } from '@/db/models';
+import { userState } from '@/store/User';
 import '@/styles/global.scss';
 
 const client = new QueryClient();
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = observer(({ Component, pageProps }: AppProps) => {
+  const [localStorageUser, setLocalStorageUser] = useLocalStorage<DBUser>('user', {
+    age: null, avatarUrl: null, city: null, email: '', id: '', name: '', posts: [], univ: null
+  })
+  useEffect(() => {
+    if (localStorageUser.id !== '') {
+      userState.setState(localStorageUser);
+    }
+  }, [])
   return (
     <>
       <QueryClientProvider client={client}>
@@ -13,4 +26,6 @@ export default function App({ Component, pageProps }: AppProps) {
       </QueryClientProvider>
     </>
   )
-}
+})
+
+export default App;
