cbdb1260e45d67c45accf2e71ec9d46a93004f2a
Yury Borets <udborets@gmail.com>
2023-05-08 21:01:57 +0300

    add header to app root

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -8,20 +8,45 @@ import { DBUser } from '@/db/models';
 import { userState } from '@/store/User';
 import '@/styles/global.scss';
 import { useUserState } from '@/hooks/useUserState';
+import Header from '@/features/Header/Header';
+import { HeaderProps } from '@/features/Header/models';
+import { useRouter } from 'next/router';
 
 const client = new QueryClient();
 
+const headerProps: HeaderProps = {
+  navLinks: [
+    {
+      href: "/auth",
+      text: "Auth",
+    },
+    {
+      href: "/posts",
+      text: "Posts",
+    },
+    {
+      href: "/friends",
+      text: "Friends",
+    },
+  ]
+}
+
 const App = observer(({ Component, pageProps }: AppProps) => {
   const { localStorageUser, setUser } = useUserState()
+  const router = useRouter();
   useEffect(() => {
     console.log(localStorageUser)
     if (localStorageUser.id !== '') {
       setUser(localStorageUser);
     }
+    if (userState.info.id === '') {
+      router.push('/auth');
+    }
   }, [])
   return (
     <>
       <QueryClientProvider client={client}>
+        <Header {...headerProps} />
         <Component {...pageProps} />
       </QueryClientProvider>
     </>
