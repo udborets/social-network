07325979544d5290d6058db183f1f3d98b9c53f2
Yury Borets <udborets@gmail.com>
2023-05-08 20:11:23 +0300

    fix post component

--- client/features/Posts/Post/Post.tsx
+++ client/features/Posts/Post/Post.tsx
@@ -7,33 +7,48 @@ import Image from "next/image";
 const Post: FC<DBPost> = (
   { id, likes, ownerId, imageUrl, likedBy, text }
 ) => {
-  const [value, setValue] = useState<boolean>(false);
+  const [value, setValue] = useState<boolean>(likedBy.includes(userState.info.id));
   const debouncedValue = useDebounce<boolean>(value, 500)
-
+  const [currentLikedBy, setCurrentLikedBy] = useState<string[]>(likedBy);
+  const [init, setInit] = useState<boolean>(true);
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.checked)
   }
 
   useEffect(() => {
-    const newLikedByList: string[] = likedBy
-      .filter((userId) => userId !== userState.info.id);
-    if (debouncedValue) {
-      newLikedByList.push(userState.info.id);
+    if (!init) {
+      const newLikedByList: string[] = likedBy
+        .filter((userId) => userId !== userState.info.id);
+      if (debouncedValue) {
+        newLikedByList.push(userState.info.id);
+      }
+      setCurrentLikedBy(newLikedByList);
+      axios.post<{ post: DBPost }>(
+        (process.env.NEXT_PUBLIC_BACKEND_URL ?? "") + '/posts/update',
+        { likedBy: newLikedByList, id: id }).then(({ data }) => {
+          console.log(data)
+          setCurrentLikedBy(data.post.likedBy);
+        });
     }
-    axios.post(
-      (process.env.NEXT_PUBLIC_BACKEND_URL ?? "") + '/posts/update',
-      { likedBy: newLikedByList, id: id })
+    if (init) {
+      setInit(false);
+    }
   }, [debouncedValue])
   return (
-    <div className="w-full max-w-[300px] bg-blue">
-      <input
-        type='checkbox'
-        className="appearance-none "
-      >
-      </input>
-      {likes}
+    <div className="w-full max-w-[300px] bg-grey outline text-white outline-[var(--blue)]">
+      <label htmlFor="like" className="checked:bg-blue">
+        haha
+        <input
+          type='checkbox'
+          name="like"
+          className="checked:bg-blue"
+          checked={value}
+          onChange={() => setValue(prev => !prev)}
+        />
+      </label>
+      {currentLikedBy?.length ?? 0}
       {imageUrl
-        ? <Image src={imageUrl} alt='post image' width={70} height={70}/>
+        ? <Image src={imageUrl} alt='post image' width={70} height={70} />
         : ''}
     </div>
   )
