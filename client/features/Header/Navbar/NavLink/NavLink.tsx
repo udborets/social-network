5056d79e5084a8edad02273fba95fc54dfc61024
Yury Borets <udborets@gmail.com>
2023-05-09 10:07:12 +0300

    fix navlink

--- client/features/Header/Navbar/NavLink/NavLink.tsx
+++ client/features/Header/Navbar/NavLink/NavLink.tsx
@@ -1,23 +1,20 @@
 import { observer } from "mobx-react-lite";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { FC } from "react";
 
-import { userState } from "@/store/User";
+import { routeState } from "@/store/Route";
 import { NavLinkProps } from "./models";
 
 const NavLink: FC<NavLinkProps> = observer(({ href, text }) => {
-  const router = useRouter();
   return (
     <Link
       href={href}
-      className={`${href === "/" + router.route.split('/')[1] ||
-        (router.asPath === `/user/${userState.info.id}` && text === 'Me')
-        ? 'text-blue-hover hover:text-blue-hover'
+      className={`${href === routeState.current
+        ? 'text-blue-hover'
         : ''} text-[1.2rem] active:text-blue-active hover:text-blue-hover duration-300 transition-all`}
     >
       {text}
-    </Link>
+    </Link >
   )
 })
 
