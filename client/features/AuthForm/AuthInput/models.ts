f0ab7596aa8727043ed1d235053d000f5a57cb31
Yury Borets <udborets@gmail.com>
2023-05-09 08:29:21 +0300

    add type for auth input props

---
+++ client/features/AuthForm/AuthInput/models.ts
@@ -0,0 +1,9 @@
+import { ErrorOption, FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+export type AuthInputProps = {
+  required?: string;
+  errors: FieldErrors<FieldValues>;
+  register: UseFormRegister<FieldValues>;
+  registerName: string;
+  text: string;
+};
