6bbc8daa5445f64ec653017f9829db5ea982746d
Yury Borets <udborets@gmail.com>
2023-05-09 13:09:27 +0300

    add check out for other users button

---
+++ client/features/CheckOutOtherUsersButton/CheckOutOtherUsersButton.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+const CheckOutOtherUsersButton = () => {
+  return (
+    <Link
+      className=""
+      href='/users'
+    >
+      Check out other users!
+    </Link>
+  )
+}
+
+export default CheckOutOtherUsersButton;
