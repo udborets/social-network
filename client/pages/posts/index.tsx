84d1534c7175b0040b2ce038290b2bb45cc482c5
Yury Borets <udborets@gmail.com>
2023-05-09 12:58:35 +0300

    fix posts page posts

--- client/pages/posts/index.tsx
+++ client/pages/posts/index.tsx
@@ -2,10 +2,31 @@ import axios from "axios";
 import { FC } from "react";
 import { useQuery } from "react-query";
 
-import { DBPost } from "@/db/models";
-import Post from "@/features/Post/Post";
+import { DBFriends, DBPost } from "@/db/models";
+import Post from "@/features/UserPosts/Post/Post";
+import { userState } from "@/store/User";
+import { observer } from "mobx-react-lite";
 
-const PostsPage: FC = () => {
+const PostsPage: FC = observer(() => {
+  const { data: friendsIds } = useQuery({
+    queryFn: async () => {
+      try {
+        const fetchedFriends = await axios.post<{ OK: boolean; friends: DBFriends[] }>((process.env.NEXT_PUBLIC_BACKEND_URL ?? "") + "/friends");
+        if (fetchedFriends.data) {
+          const allFriends = fetchedFriends.data.friends
+            .filter(({ friendId, userId }) => [friendId, userId].includes(userState.info.id))
+            .map(({ friendId, userId }) => [friendId, userId]);
+          const allIds: string[] = [];
+          allFriends.forEach((idsArr) => allIds.push(...idsArr));
+          return allIds;
+        }
+      }
+      catch (e) {
+        console.error(e);
+      }
+    },
+    queryKey: [`postPageFriends${userState.info.id}`],
+  })
   const { data: posts } = useQuery({
     queryFn: async () => {
       try {
@@ -22,15 +43,37 @@ const PostsPage: FC = () => {
   })
   return (
     <main className="w-full h-full flex flex-col gap-4 justify-start items-center">
-      {(posts && posts.length !== 0) ? posts
+      {(posts && posts.length !== 0 && posts.filter((post) => {
+        if (friendsIds) {
+          if (friendsIds.includes(post.ownerId)) {
+            return true;
+          }
+          return false;
+        }
+        return true;
+      })
         .sort((b, a) => (new Date(a.createdAt ?? Date.now())).getTime()
           - (new Date(b.createdAt ?? Date.now())).getTime())
         .map((postProps) => (
           <Post {...postProps} key={postProps.id} />
-        ))
-        : ''}
+        )).length !== 0)
+        ? posts.filter((post) => {
+          if (friendsIds) {
+            if (friendsIds.includes(post.ownerId)) {
+              return true;
+            }
+            return false;
+          }
+          return true;
+        })
+          .sort((b, a) => (new Date(a.createdAt ?? Date.now())).getTime()
+            - (new Date(b.createdAt ?? Date.now())).getTime())
+          .map((postProps) => (
+            <Post {...postProps} key={postProps.id} />
+          ))
+        : `Your friends did not post anything yet`}
     </main>
   )
-}
+})
 
 export default PostsPage;
