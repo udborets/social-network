db19e679a15759a8eceeac2d70ada353217085c3
Yury Borets <udborets@gmail.com>
2023-05-10 11:09:31 +0300

    add info input component

---
+++ client/features/UserInfo/InfoForm/InfoInput/models.ts
@@ -0,0 +1,10 @@
+import { HTMLInputTypeAttribute } from "react";
+import { FieldValues, UseFormRegister } from "react-hook-form";
+
+export type InfoInputProps = {
+  registerFn: UseFormRegister<FieldValues>;
+  name: string;
+  text: string;
+  placeholder: string;
+  type: HTMLInputTypeAttribute;
+};
