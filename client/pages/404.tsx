fef80091f3d2cab557cf278430ed7c2db68db352
Yury Borets <udborets@gmail.com>
2023-05-10 20:56:35 +0300

    fix not found page styles

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -5,7 +5,7 @@ import BackToButton from "@/features/BackToButton/BackToButton";
 const NotFoundPage: FC = () => {
   return (
     <main className="grid place-items-center min-h-[calc(100vh-100px)]">
-      <div className="flex flex-col">
+      <div className="flex flex-col justify-center items-center gap-8 font-bold text-[2rem]">
         <h1>
           Oops!
         </h1>
@@ -14,6 +14,7 @@ const NotFoundPage: FC = () => {
         </h2>
         <BackToButton
           text="Back to the Home page!"
+          className="text-[1rem] font-normal"
           href={`/`}
         />
       </div>
