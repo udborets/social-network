d07d8c494b4cc853a956e87400cc598feae8e9da
Yury Borets <udborets@gmail.com>
2023-05-10 16:10:12 +0300

    fix spinner modal window css position type

--- client/features/SpinnerModalWindow/SpinnerModalWindow.tsx
+++ client/features/SpinnerModalWindow/SpinnerModalWindow.tsx
@@ -4,7 +4,7 @@ import styles from './styles.module.scss';
 
 const SpinnerModalWindow = () => {
   return (
-    <div className={`absolute w-screen h-screen left-0 top-0 grid place-items-center z-[10] bg-[#00000086]`}>
+    <div className={`fixed w-screen h-screen left-0 top-0 grid place-items-center z-[10] bg-[#00000086]`}>
       <div className={`${styles.spinner}`} />
     </div>
   )
