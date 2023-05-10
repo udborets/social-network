2330ada6110d22819e673477e1dfa402284852d7
Yury Borets <udborets@gmail.com>
2023-05-10 08:35:11 +0300

    fix user item queries and styles

--- client/features/UserItem/UserItem.tsx
+++ client/features/UserItem/UserItem.tsx
@@ -7,9 +7,10 @@ import { DBFriends } from "@/db/models";
 import { userState } from "@/store/User";
 import { UserItemProps } from "./models";
 
-const UserItem: FC<UserItemProps> = observer(({ id }) => {
+const UserItem: FC<UserItemProps> = observer(({ id, name }) => {
   const [isFriend, setIsFriend] = useState<boolean>(false);
-  const { refetch, isFetching, isRefetching } = useQuery({
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { refetch } = useQuery({
     queryFn: async () => {
       try {
         const fetchedFriends = await axios.post<{ friends: DBFriends[] }>((process.env.NEXT_PUBLIC_BACKEND_URL ?? "") + '/friends');
@@ -29,21 +30,26 @@ const UserItem: FC<UserItemProps> = observer(({ id }) => {
     refetch();
   }, [])
   return (
-    <div className="w-full max-w-[500px] max-h-[100px] h-full bg-slate-300 rounded-[20px] p-5 flex gap-4">
-      <span>{id}</span>
+    <div className="w-full max-w-[500px] max-h-[100px] h-full bg-slate-300 rounded-[20px] p-5 flex gap-4 justify-between items-center">
+      <span className="font-bold text-[1.4rem] w-full text-center">
+        {name}
+      </span>
       <button
-        disabled={isFetching || isRefetching}
+        disabled={isLoading}
         className={`${isFriend
           ? 'hover:bg-red-600 active:bg-red-900 outline-red-400'
           : 'hover:bg-blue-hover active:bg-blue-active outline-[var(--blue)]'} disabled:bg-[#00000096] outline rounded-[10px] p-2 bg-white 
-          duration-300 transition-all hover:text-white active:text-white`}
+          duration-300 transition-all hover:text-white active:text-white min-h-[50px] min-w-[150px]`}
         onClick={async () => {
           if (!isFriend) {
-            const a = await axios.post((process.env.NEXT_PUBLIC_BACKEND_URL ?? "") + '/friends/add', { userId: userState.info.id, friendId: id })
-            console.log(a)
+            setIsLoading(true);
+            await axios.post((process.env.NEXT_PUBLIC_BACKEND_URL ?? "") + '/friends/add', { userId: userState.info.id, friendId: id });
+            setIsLoading(false);
           }
           if (isFriend) {
-            await axios.post((process.env.NEXT_PUBLIC_BACKEND_URL ?? "") + '/friends/remove', { userId: userState.info.id, friendId: id })
+            setIsLoading(true);
+            await axios.post((process.env.NEXT_PUBLIC_BACKEND_URL ?? "") + '/friends/remove', { userId: userState.info.id, friendId: id });
+            setIsLoading(false);
           }
           refetch();
         }}
