88e2c7aab9c5cc8d8fce8a0ea7132d3b4f5e501d
Yury Borets <udborets@gmail.com>
2023-05-10 14:57:26 +0300

    fix posts width

--- client/features/UserPosts/UserPosts.tsx
+++ client/features/UserPosts/UserPosts.tsx
@@ -5,7 +5,7 @@ import Post from "./Post/Post";
 
 const UserPosts: FC<UserPostsProps> = ({ posts }) => {
   return (
-    <ul className="flex flex-col gap-8 max-w-[80%] w-full rounded-[10px] bg-slate-50 p-[10px]">
+    <ul className="flex flex-col gap-8 max-w-[700px] w-full rounded-[10px] bg-slate-50 p-[10px]">
       {posts
         .sort((b, a) => (new Date(a.createdAt ?? Date.now())).getTime()
           - (new Date(b.createdAt ?? Date.now())).getTime())
