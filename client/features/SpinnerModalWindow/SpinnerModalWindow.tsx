9a65c27c903342f503a87107352eac3ccc55efe6
Yury Borets <udborets@gmail.com>
2023-05-10 16:08:36 +0300

    add spinner modal window component

---
+++ client/features/SpinnerModalWindow/SpinnerModalWindow.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+const SpinnerModalWindow = () => {
+  return (
+    <div className={`absolute w-screen h-screen left-0 top-0 grid place-items-center z-[10] bg-[#00000086]`}>
+      <div className={`${styles.spinner}`} />
+    </div>
+  )
+}
+
+export default SpinnerModalWindow
