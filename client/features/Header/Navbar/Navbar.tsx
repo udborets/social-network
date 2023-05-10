bf9228c7cd1c5ae5591495186e922de095f06be8
Yury Borets <udborets@gmail.com>
2023-05-10 08:34:51 +0300

    fix navbar styles

--- client/features/Header/Navbar/Navbar.tsx
+++ client/features/Header/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import { NavbarProps } from "./models";
 const Navbar: FC<NavbarProps> = observer(({ navLinks }) => {
   return (
     <nav className="w-fit">
-      <ul className="flex justify-evenly w-fit gap-8">
+      <ul className="flex justify-evenly w-fit gap-3 sm:gap-6 md:gap-8 flex-wrap">
         {userState.info.id !== ''
           ? [...navLinks, { href: `/users/${userState.info.id}`, text: "Me" }].map((navLinkProps) => (
             <NavLink
